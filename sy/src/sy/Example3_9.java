a807fb8a4e37f7a09e6fb4746e8364b600a23511
cxzw <39974534+cxzw@users.noreply.github.com>
2018-06-06 22:22:10 +0800

    Add files via upload

---
+++ sy/src/sy/Example3_9.java
@@ -0,0 +1,21 @@
+package sy;
+
+import java.util.*;
+
+public class Example3_9 {
+	public static void main(String args[])
+	{
+		Scanner reader = new Scanner(System.in);
+		double sum = 0;
+		int m = 0;
+		
+		while (reader.hasNextDouble())
+		{
+			double x = reader.nextDouble();
+			m += 1;
+			sum = sum+x;
+		}
+		System.out.printf("%d个数的和为%f\n",m,sum);
+		System.out.printf("%d个数的平均值为%f\n",m,sum/m);
+	}
+}
